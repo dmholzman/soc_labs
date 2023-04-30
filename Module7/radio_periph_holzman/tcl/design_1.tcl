0c45987f9407b1d2de162c5427c2e235594a83c8
Dirk Holzman <dirk.holzman@gmail.com>
2023-04-30 16:53:21 -0400

    Project finished and can be built from .bat file.

--- Module7/radio_periph_holzman/tcl/design_1.tcl
+++ Module7/radio_periph_holzman/tcl/design_1.tcl
@@ -124,9 +124,10 @@ set bCheckIPs 1
 if { $bCheckIPs == 1 } {
    set list_check_ips "\ 
 xilinx.com:ip:axi_iic:2.1\
-jhu.edu:user:full_radio:1.0\
+jhu.edu:user:full_radio:2.0\
 jhuapl.edu:user:lowlevel_dac_intfc:1.1\
 xilinx.com:ip:processing_system7:5.5\
+xilinx.com:user:radio_fifo:1.1\
 xilinx.com:ip:proc_sys_reset:5.0\
 xilinx.com:ip:system_ila:1.1\
 xilinx.com:ip:xpm_cdc_gen:1.0\
@@ -219,12 +220,12 @@ proc create_root_design { parentCell } {
  ] $axi_iic_0
 
   # Create instance: full_radio_0, and set properties
-  set full_radio_0 [ create_bd_cell -type ip -vlnv jhu.edu:user:full_radio:1.0 full_radio_0 ]
+  set full_radio_0 [ create_bd_cell -type ip -vlnv jhu.edu:user:full_radio:2.0 full_radio_0 ]
 
   # Create instance: lowlevel_dac_intfc_0, and set properties
   set lowlevel_dac_intfc_0 [ create_bd_cell -type ip -vlnv jhuapl.edu:user:lowlevel_dac_intfc:1.1 lowlevel_dac_intfc_0 ]
   set_property -dict [ list \
-   CONFIG.channels {1} \
+   CONFIG.channels {2} \
  ] $lowlevel_dac_intfc_0
 
   # Create instance: processing_system7_0, and set properties
@@ -712,8 +713,11 @@ gpio[0]#qspi0_ss_b#qspi0_io[0]#qspi0_io[1]#qspi0_io[2]#qspi0_io[3]/HOLD_B#qspi0_
   # Create instance: ps7_0_axi_periph, and set properties
   set ps7_0_axi_periph [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 ps7_0_axi_periph ]
   set_property -dict [ list \
-   CONFIG.NUM_MI {2} \
+   CONFIG.NUM_MI {3} \
  ] $ps7_0_axi_periph
+
+  # Create instance: radio_fifo_0, and set properties
+  set radio_fifo_0 [ create_bd_cell -type ip -vlnv xilinx.com:user:radio_fifo:1.1 radio_fifo_0 ]
 
   # Create instance: rst_ps7_0_125M, and set properties
   set rst_ps7_0_125M [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 rst_ps7_0_125M ]
@@ -739,17 +743,18 @@ gpio[0]#qspi0_ss_b#qspi0_io[0]#qspi0_io[1]#qspi0_io[2]#qspi0_io[3]/HOLD_B#qspi0_
 
   # Create interface connections
   connect_bd_intf_net -intf_net axi_iic_0_IIC [get_bd_intf_ports IIC_0] [get_bd_intf_pins axi_iic_0/IIC]
-  connect_bd_intf_net -intf_net dds_compiler_0_M_AXIS_DATA [get_bd_intf_pins full_radio_0/m_axis] [get_bd_intf_pins lowlevel_dac_intfc_0/data_in]
-connect_bd_intf_net -intf_net [get_bd_intf_nets dds_compiler_0_M_AXIS_DATA] [get_bd_intf_pins lowlevel_dac_intfc_0/data_in] [get_bd_intf_pins system_ila_0/SLOT_0_AXIS]
   connect_bd_intf_net -intf_net processing_system7_0_DDR [get_bd_intf_ports DDR] [get_bd_intf_pins processing_system7_0/DDR]
   connect_bd_intf_net -intf_net processing_system7_0_FIXED_IO [get_bd_intf_ports FIXED_IO] [get_bd_intf_pins processing_system7_0/FIXED_IO]
   connect_bd_intf_net -intf_net processing_system7_0_M_AXI_GP0 [get_bd_intf_pins processing_system7_0/M_AXI_GP0] [get_bd_intf_pins ps7_0_axi_periph/S00_AXI]
   connect_bd_intf_net -intf_net ps7_0_axi_periph_M00_AXI [get_bd_intf_pins full_radio_0/S00_AXI] [get_bd_intf_pins ps7_0_axi_periph/M00_AXI]
 connect_bd_intf_net -intf_net [get_bd_intf_nets ps7_0_axi_periph_M00_AXI] [get_bd_intf_pins ps7_0_axi_periph/M00_AXI] [get_bd_intf_pins system_ila_0/SLOT_1_AXI]
   connect_bd_intf_net -intf_net ps7_0_axi_periph_M01_AXI [get_bd_intf_pins axi_iic_0/S_AXI] [get_bd_intf_pins ps7_0_axi_periph/M01_AXI]
+  connect_bd_intf_net -intf_net ps7_0_axi_periph_M02_AXI [get_bd_intf_pins ps7_0_axi_periph/M02_AXI] [get_bd_intf_pins radio_fifo_0/S00_AXI]
 
   # Create port connections
   connect_bd_net -net axi_iic_0_gpo [get_bd_pins axi_iic_0/gpo] [get_bd_pins lowlevel_dac_intfc_0/resetn]
+  connect_bd_net -net full_radio_0_m_axis_tdata [get_bd_pins full_radio_0/m_axis_tdata] [get_bd_pins lowlevel_dac_intfc_0/data_word] [get_bd_pins radio_fifo_0/s_axis_tdata] [get_bd_pins system_ila_0/SLOT_0_AXIS_tdata]
+  connect_bd_net -net full_radio_0_m_axis_tvalid [get_bd_pins full_radio_0/m_axis_tvalid] [get_bd_pins lowlevel_dac_intfc_0/valid] [get_bd_pins radio_fifo_0/s_axis_tvalid] [get_bd_pins system_ila_0/SLOT_0_AXIS_tvalid]
   connect_bd_net -net lowlevel_dac_intfc_0_bclk [get_bd_ports bclk] [get_bd_pins lowlevel_dac_intfc_0/bclk]
   connect_bd_net -net lowlevel_dac_intfc_0_lrck [get_bd_ports lrck] [get_bd_pins lowlevel_dac_intfc_0/lrck]
   connect_bd_net -net lowlevel_dac_intfc_0_mclk [get_bd_ports mclk] [get_bd_pins lowlevel_dac_intfc_0/mclk]
@@ -758,12 +763,13 @@ connect_bd_intf_net -intf_net [get_bd_intf_nets ps7_0_axi_periph_M00_AXI] [get_b
   connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_ps7_0_125M/ext_reset_in]
   connect_bd_net -net rst_ps7_0_125M_peripheral_aresetn [get_bd_pins ps7_0_axi_periph/ARESETN] [get_bd_pins ps7_0_axi_periph/S00_ARESETN] [get_bd_pins rst_ps7_0_125M/peripheral_aresetn] [get_bd_pins xpm_cdc_gen_0/src_rst]
   connect_bd_net -net rst_ps7_0_125M_peripheral_reset [get_bd_pins rst_ps7_0_125M/peripheral_reset]
-  connect_bd_net -net s00_axi_aclk_0_1 [get_bd_ports clk125] [get_bd_pins axi_iic_0/s_axi_aclk] [get_bd_pins full_radio_0/s00_axi_aclk] [get_bd_pins lowlevel_dac_intfc_0/clk125] [get_bd_pins ps7_0_axi_periph/M00_ACLK] [get_bd_pins ps7_0_axi_periph/M01_ACLK] [get_bd_pins system_ila_0/clk] [get_bd_pins xpm_cdc_gen_0/dest_clk]
-  connect_bd_net -net xpm_cdc_gen_0_dest_rst_out [get_bd_pins axi_iic_0/s_axi_aresetn] [get_bd_pins full_radio_0/s00_axi_aresetn] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN] [get_bd_pins system_ila_0/resetn] [get_bd_pins xpm_cdc_gen_0/dest_rst_out]
+  connect_bd_net -net s00_axi_aclk_0_1 [get_bd_ports clk125] [get_bd_pins axi_iic_0/s_axi_aclk] [get_bd_pins full_radio_0/s00_axi_aclk] [get_bd_pins lowlevel_dac_intfc_0/clk125] [get_bd_pins ps7_0_axi_periph/M00_ACLK] [get_bd_pins ps7_0_axi_periph/M01_ACLK] [get_bd_pins ps7_0_axi_periph/M02_ACLK] [get_bd_pins radio_fifo_0/s00_axi_aclk] [get_bd_pins system_ila_0/clk] [get_bd_pins xpm_cdc_gen_0/dest_clk]
+  connect_bd_net -net xpm_cdc_gen_0_dest_rst_out [get_bd_pins axi_iic_0/s_axi_aresetn] [get_bd_pins full_radio_0/s00_axi_aresetn] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN] [get_bd_pins ps7_0_axi_periph/M02_ARESETN] [get_bd_pins radio_fifo_0/s00_axi_aresetn] [get_bd_pins system_ila_0/resetn] [get_bd_pins xpm_cdc_gen_0/dest_rst_out]
 
   # Create address segments
   assign_bd_address -offset 0x41600000 -range 0x00010000 -target_address_space [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs axi_iic_0/S_AXI/Reg] -force
   assign_bd_address -offset 0x43C00000 -range 0x00010000 -target_address_space [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs full_radio_0/S00_AXI/S00_AXI_reg] -force
+  assign_bd_address -offset 0x43C10000 -range 0x00010000 -target_address_space [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs radio_fifo_0/S00_AXI/S00_AXI_reg] -force
 
 
   # Restore current instance
